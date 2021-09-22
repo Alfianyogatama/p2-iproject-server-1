5431c08288f3bab307dc04942242551fd440a22e
Alfian Rudi Yogatama <alvmarjinal@gmail.com>
2021-09-22 11:49:10 +0700

    Add football standing info

--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const chatRoutes = require('./chatRoutes')
+const featureRoutes = require('./featureRoutes')
 
 router.use('/', chatRoutes)
+router.use('/feature', featureRoutes)
 
 module.exports = router
